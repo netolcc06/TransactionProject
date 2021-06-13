4e970b42f6e57308e251c34727601f155e80c6a5
netolcc06 <netolcc06@gmail.com>
2021-06-13 16:35:34 -0700

    Transactions with amount equals to zero are not allowed anymore

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
-  validates :amount, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than_or_equal_to: 0}
+  validates :amount, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}
   validates :source_id, :destiny_id, presence: true
   belongs_to :source, class_name: "Account", foreign_key: "source_id"
   belongs_to :destiny, class_name: "Account", foreign_key: "destiny_id"
